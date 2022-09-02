2549f8966995f180a5c5c373a3ca1b404c91d730
Vlad Gheorghiu <vsoftco@gmail.com>
2022-09-02 16:06:36 -0400

    Update format_results.sh

--- results/approx_algo1/format_results.sh
+++ results/approx_algo1/format_results.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
 
+# $1 - Rotation name (e.g., 'rzk')
+# $2 - Precision (epsilon) (e.g., 1e-2)
+
+# Formats the raw data produced by run_approx_algo1_count[_MPI].sh
+
+if [[ $# -lt 2 ]]; then
+    printf "Usage: %s <rotation_name> <epsilon>\n" "$0"
+    exit 1
+fi
+
 name=$1
 error=$2
 
